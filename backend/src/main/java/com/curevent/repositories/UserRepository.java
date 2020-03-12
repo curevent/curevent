f7140c2b71a2556e9e5a7970c7b5f001610bffda
alskaa <alisagorodeckaa7786@gmail.com>
2020-03-12 15:02:33 +0300

    small fixed repository

--- backend/src/main/java/com/curevent/repositories/UserRepository.java
+++ backend/src/main/java/com/curevent/repositories/UserRepository.java
@@ -4,10 +4,8 @@ import com.curevent.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    List <User> findById(UUID id);
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
