9310e546bca9a2fcd19bfd406e133a77bb5ae1bd
alskaa <alisagorodeckaa7786@gmail.com>
2020-03-13 13:45:53 +0300

    user validation added

--- backend/src/main/java/com/curevent/controllers/UserController.java
+++ backend/src/main/java/com/curevent/controllers/UserController.java
@@ -2,28 +2,36 @@ package com.curevent.controllers;
 
 import com.curevent.models.User;
 import com.curevent.services.UserService;
+import com.curevent.utils.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
 public class UserController {
 
     private final UserService userService;
+    private final UserValidator userValidator;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserValidator userValidator) {
         this.userService = userService;
+        this.userValidator = userValidator;
     }
+
     @GetMapping("/users/{id}")
     public User getUser(@PathVariable UUID id) {
-
         return userService.getOneById(id);
     }
 
     @PostMapping("/users/add")
-    public void addUser(@RequestBody User user) {
-        userService.add(user);
+    public void addUser(@RequestBody @Valid User user, BindingResult result) {
+        userValidator.validate(user, result);
+        if (!result.hasErrors()) {
+            userService.add(user);
+        }
     }
 }
