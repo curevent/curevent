e4fc6634bd15c01c30bf38fc3fb206abd1239726
Hardelele <Hardelele@yahoo.com>
2020-03-16 23:46:19 +0300

    Implement UserDetailsService

---
+++ backend/src/main/java/com/curevent/services/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.curevent.services;
+
+import com.curevent.models.entities.UserEntity;
+import com.curevent.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User by username " + username + " not found"));
+
+        return User
+                .withUsername(username)
+                .password(userEntity.getPassword())
+                //.authorities(userEntity.getRoles())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
