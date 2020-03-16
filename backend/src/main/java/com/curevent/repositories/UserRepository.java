4b8556ebf967e2c2e9d81b6ef93d7c0a67ae936a
Hardelele <Hardelele@yahoo.com>
2020-03-16 23:44:22 +0300

    Add new Username and Email queries

--- backend/src/main/java/com/curevent/repositories/UserRepository.java
+++ backend/src/main/java/com/curevent/repositories/UserRepository.java
@@ -4,10 +4,17 @@ import com.curevent.models.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
-    UserEntity findByEmail(String email);
-    UserEntity findByUsername(String username);
+
+    Optional<UserEntity> findByEmail(String email);
+
+    Optional<UserEntity> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
