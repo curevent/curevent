4be718cabdeee2ee6b8b00eed932b811b82ebc91
Hardelele <Hardelele@yahoo.com>
2020-03-16 23:45:50 +0300

    Start to impl AuthService

---
+++ backend/src/main/java/com/curevent/services/AuthService.java
@@ -0,0 +1,126 @@
+package com.curevent.services;
+
+import com.curevent.exceptions.AuthenticationException;
+import com.curevent.models.entities.UserEntity;
+import com.curevent.models.forms.LoginForm;
+import com.curevent.models.forms.RegisterForm;
+import com.curevent.models.transfers.AuthTransfer;
+import com.curevent.repositories.UserRepository;
+import com.curevent.security.TokenProvider;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Transactional
+@AllArgsConstructor
+@Slf4j
+public class AuthService {
+
+    private final TokenProvider tokenProvider;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthTransfer register(RegisterForm registerForm) {
+        // checking that the user exists in the database
+        if (!userRepository.existsByUsername(registerForm.getUsername())) {
+
+            // create a new refresh token
+            String refreshToken = tokenProvider.createRefreshToken();
+
+            // create instance of user model and fill it
+            UserEntity userModel = UserEntity.builder()
+                    .id(UUID.randomUUID())
+                    .username(registerForm.getUsername())
+                    .email(registerForm.getEmail())
+                    .password(passwordEncoder.encode(registerForm.getPassword()))
+                    .refreshToken(refreshToken)
+                    .build();
+
+            // save the user in database
+            userRepository.save(userModel);
+
+            // create tokens transfer object and return it
+            return AuthTransfer.builder()
+                    //.accessToken(tokenProvider.createAccessToken(userModel.getUsername(), userModel.getRoles()))
+                    .refreshToken(refreshToken)
+                    .tokenType(tokenProvider.getTokenType())
+                    .expiresIn(tokenProvider.getExpire())
+                    .build();
+        } else {
+            throw new AuthenticationException("Username already exists", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public AuthTransfer login(LoginForm loginForm) {
+        try {
+            // find user from database
+            UserEntity userModel = userRepository.findByUsername(loginForm.getUsername()).orElseThrow(() ->
+                    new NotFoundException("The user by username [" + loginForm.getUsername() + "] not found",
+                            HttpStatus.NOT_FOUND));
+
+            // create a new refresh token
+            String refreshToken = tokenProvider.createRefreshToken();
+
+            // update refresh token
+            userModel.setRefreshToken(refreshToken);
+
+            // update user details in database
+            userRepository.save(userModel);
+
+            // authenticate this user in the authentication manager
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginForm.getUsername(),
+                    loginForm.getPassword()));
+
+            // create tokens transfer object and return it
+            return AuthTransfer.builder()
+                    .accessToken(tokenProvider.createAccessToken(userModel.getUsername(), userModel.getRoles()))
+                    .refreshToken(refreshToken)
+                    .tokenType(tokenProvider.getTokenType())
+                    .expiresIn(tokenProvider.getExpire())
+                    .build();
+        } catch (AuthenticationException e) {
+            throw new AuthenticationException("Invalid username or password supplied", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public UserEntity whoami(HttpServletRequest request) {
+        // get username from jwt token
+        String username = tokenProvider.getUsernameByToken(tokenProvider.extractToken(request));
+
+        // find user by username from database
+        UserEntity userModel = userRepository.findByUsername(username)
+                .orElseThrow(() -> new NotFoundException("The user by username [" + username + "] not found",
+                        HttpStatus.NOT_FOUND));
+
+        return userModel;
+    }
+
+    public AuthTransfer refresh(String username, String refreshToken) {
+        // get instance of user model by username from database
+        Optional<UserEntity> userModel = userRepository.findByUsername(username);
+
+        // check for equality of refresh tokens
+        if (userModel.isPresent() && userModel.get().getRefreshToken().equals(refreshToken)) {
+
+            return AuthTransfer.builder()
+                    .accessToken(tokenProvider.createAccessToken(username, userModel.get().getRoles()))
+                    .refreshToken(tokenProvider.createRefreshToken())
+                    .tokenType(tokenProvider.getTokenType())
+                    .expiresIn(tokenProvider.getExpire())
+                    .build();
+        } else {
+            throw new AuthenticationException("Invalid refresh token", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
