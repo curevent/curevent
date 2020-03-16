796166a900e3b9bb59fb35367ff257bbf36540cb
Hardelele <Hardelele@yahoo.com>
2020-03-17 02:40:07 +0300

    Handling NotFoundException

--- backend/src/main/java/com/curevent/exceptions/handlers/AuthExceptionHandler.java
+++ backend/src/main/java/com/curevent/exceptions/handlers/AuthExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.curevent.exceptions.handlers;
 
 import com.curevent.exceptions.AuthenticationException;
+import com.curevent.exceptions.NotFoundException;
 import com.curevent.models.transfers.ErrorTransfer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,11 @@ public class AuthExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<?> handleAuthenticationException(AuthenticationException e) {
+        return new ResponseEntity<>(new ErrorTransfer(e.getMessage(), e.getHttpStatus().value()), e.getHttpStatus());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(NotFoundException e) {
         return new ResponseEntity<>(new ErrorTransfer(e.getMessage(), e.getHttpStatus().value()), e.getHttpStatus());
     }
 }
