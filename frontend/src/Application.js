67dce1b0d91aff67b2703d808fa8eeccf1687273
Hardelele <Hardelele@yahoo.com>
2020-03-26 11:11:44 +0300

    Add Route to /auth

--- frontend/src/Application.js
+++ frontend/src/Application.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {AuthPage} from "./pages/auth/AuthPage";
 
 function App() {
   return (
       <BrowserRouter>
           <div className="main-application-container">
               <Switch>
-
+                <Route path="/auth" exact component={AuthPage}/>
               </Switch>
           </div>
       </BrowserRouter>
