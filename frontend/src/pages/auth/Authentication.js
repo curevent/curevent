d52b8bf258e419ebf3217eca9dee3c7745a321a3
Hardelele <Hardelele@yahoo.com>
2020-03-26 05:56:45 +0300

    Add auth form

--- frontend/src/pages/auth/Authentication.js
+++ frontend/src/pages/auth/Authentication.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import '../../css/authentication.css'
+import React, {useState} from 'react';
 
 export const Authentication = () => {
+
+    const [login, setLogin] = useState('login');
+
+    const [password, setPassword] = useState('password');
+
+    const submitHandler = event => {
+    };
+
     return (
         <div className="authentication-container">
-            <input id="login" className="auth-input"/>
-            <input id="password" className="auth-input"/>
-            <button className="auth-button" type="submit">Login</button>
+            <h1 className="title">Authentication</h1>
+            <input
+                type="text"
+                id="login"
+                className="auth-input"
+                value={login.valueOf()}
+                onChange={event => setLogin(event.target.value)}
+                onFocus={() => setLogin('')}
+            />
+            <input
+                type="password"
+                id="password"
+                className="auth-input"
+                value={password.valueOf()}
+                onChange={event => setPassword(event.target.value)}
+                onFocus={() => setPassword('')}
+            />
+            <button
+                className="auth-button"
+                type="submit"
+                onClick={submitHandler}
+            >
+                Login
+            </button>
         </div>
     );
 };
