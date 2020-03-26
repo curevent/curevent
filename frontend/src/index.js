ef21bb2a5e0cba8f34e9e22de447e023c7685d69
Hardelele <Hardelele@yahoo.com>
2020-03-26 11:09:47 +0300

    Add store with rootReducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Application';
 import * as serviceWorker from './serviceWorker';
+import {compose, createStore} from "redux";
+import {rootReducer} from "./redux/reducers/RootReducer";
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
 
 serviceWorker.unregister();
