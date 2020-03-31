1b0c91e68913fe8b22d79b9d8cc3a0eceeb4686f
Hardelele <Hardelele@yahoo.com>
2020-03-31 16:33:09 +0300

    Impl auth() and add initialState

---
+++ frontend/src/redux/reducers/AuthReducer.js
@@ -0,0 +1,20 @@
+import {AUTH_ACTION, REGISTER_ACTION} from "../actions/ActionTypes";
+import {postAuth, postRegister} from "../actions/AuthActions";
+
+const initialState = {
+    auth_token: null,
+    refresh_token: null,
+    token_type: "bearer ",
+    expires_in: 0
+};
+
+export function auth(state = initialState, action) {
+    switch (action.type) {
+        case AUTH_ACTION:
+            return postAuth(action.authInfo);
+        case REGISTER_ACTION:
+            return postRegister(action.registerInfo);
+        default:
+            return state;
+    }
+}
