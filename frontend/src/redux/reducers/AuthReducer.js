7ac7f9a28e4c4634a71227ff218020a233882cf2
Hardelele <Hardelele@yahoo.com>
2020-04-02 18:34:57 +0300

    Fix auth reducer

--- frontend/src/redux/reducers/AuthReducer.js
+++ frontend/src/redux/reducers/AuthReducer.js
@@ -2,18 +2,16 @@ import {AUTH_ACTION, REGISTER_ACTION} from "../actions/ActionTypes";
 import {postAuth, postRegister} from "../actions/AuthActions";
 
 const initialState = {
-    auth_token: null,
-    refresh_token: null,
-    token_type: "bearer ",
-    expires_in: 0
+    tokens:{}
 };
 
 export function authReducer(state = initialState, action) {
+    console.log(action);
     switch (action.type) {
         case AUTH_ACTION:
-            return postAuth(action.authInfo);
+            return {...state, tokens: action.payload};
         case REGISTER_ACTION:
-            return postRegister(action.registerInfo);
+
         default:
             return state;
     }
