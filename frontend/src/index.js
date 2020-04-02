ccc167c429e3b5871d01fb6ebdffe0b9ffa2ee92
Hardelele <Hardelele@yahoo.com>
2020-04-02 18:33:55 +0300

    applyMiddleware(thunk)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Application';
 import * as serviceWorker from './serviceWorker';
-import {compose, createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import {rootReducer} from "./redux/reducers/RootReducer";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
 
 const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
