97af7b194d144cbb65df2c38231477f3ad030b60
Hardelele <Hardelele@yahoo.com>
2020-04-03 05:13:21 +0300

    Add Registration action

--- frontend/src/pages/auth/Registration.js
+++ frontend/src/pages/auth/Registration.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
+import {postAuth, postRegister} from "../../redux/actions/AuthActions";
 
 export const Registration = () => {
+
+    const dispatch = useDispatch()
+
+    const [registration, setRegistration] = useState({email:"", username:"", password:""});
+
+    const submitHandler = event => {
+        const {email, username, password} = registration;
+        const registrationInfo = {email, username, password};
+        console.log(registrationInfo);
+        dispatch(postRegister(registrationInfo));
+        setRegistration({email:"", username:"", password:""});
+    };
+
+    const changeInputHandler = event => {
+        event.persist();
+        setRegistration(pervState => ( { ...pervState, ...{
+                [event.target.id]: event.target.value
+            }}))
+    };
+
     return (
         <div className="registration-container">
             <h1 className="title">Registration</h1>
-            <input type="text" id="login" className="auth-input"/>
-            <input type="text" id="email" className="auth-input"/>
-            <input type="password" id="password" className="auth-input"/>
-            <button className="auth-button" type="submit">Register</button>
+            <input
+                type="text"
+                id="username"
+                className="auth-input"
+                placeholder="Username"
+                value={registration.username}
+                onChange={changeInputHandler}
+            />
+            <input
+                type="text"
+                id="email"
+                className="auth-input"
+                placeholder="Email"
+                value={registration.email}
+                onChange={changeInputHandler}
+            />
+            <input
+                type="password"
+                id="password"
+                className="auth-input"
+                placeholder="Password"
+                value={registration.password}
+                onChange={changeInputHandler}
+            />
+            <button
+                className="auth-button"
+                type="submit"
+                onClick={submitHandler}
+            >Register</button>
         </div>
     );
 };
