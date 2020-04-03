3564482623282d724bfb8e9e917137b8f2ad2880
Hardelele <Hardelele@yahoo.com>
2020-04-03 05:11:42 +0300

    Set main block class to application-container

--- frontend/src/Application.js
+++ frontend/src/Application.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {AuthPage} from "./pages/auth/AuthPage";
+import './css/application.css'
 
 function App() {
   return (
       <BrowserRouter>
-          <div className="main-application-container">
+          <div className="application-container">
               <Switch>
                 <Route path="/auth" exact component={AuthPage}/>
               </Switch>
