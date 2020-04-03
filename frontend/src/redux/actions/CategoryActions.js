f1f645b9e7225707ca100d783c8beff74be59010
alskaa <alisagorodeckaa7786@gmail.com>
2020-04-03 21:49:48 +0300

    add comment and relationship actions and types

--- frontend/src/redux/actions/CategoryActions.js
+++ frontend/src/redux/actions/CategoryActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {POST_CATEGORY, GET_CATEGORY_BY_ID, PUT_CATEGORY, DELETE_CATEGORY_BY_ID } from "../api/ApiEndpoints";
 import {POST_CATEGORY_ACTION, GET_CATEGORY_ACTION, PUT_CATEGORY_ACTION, DELETE_CATEGORY_ACTION} from "./ActionTypes";
 
-export function postTag(category, token) {
+export function postCategory(category, token) {
     return dispatch => {
         axios.post(POST_CATEGORY, category, {
             headers: {'Authorization':`Bearer ${token}`}
---
+++ frontend/src/redux/actions/CommentActions.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import {POST_COMMENT, GET_COMMENT_BY_ID, PUT_COMMENT, DELETE_COMMENT_BY_ID } from "../api/ApiEndpoints";
+import {POST_COMMENT_ACTION, GET_COMMENT_ACTION, PUT_COMMENT_ACTION, DELETE_COMMENT_ACTION} from "./ActionTypes";
+
+export function postComment(comment, token) {
+    return dispatch => {
+        axios.post(POST_COMMENT, comment, {
+            headers: {'Authorization':`Bearer ${token}`}
+        }).then(response => {
+            dispatch({type: POST_COMMENT_ACTION, payload: response.data})
+        });
+    }
+}
+
+export function getComment(id, token) {
+    return dispatch => {
+        axios.get(GET_COMMENT_BY_ID(id), {
+            headers: {'Authorization':`Bearer ${token}`}
+        }).then(response => {
+            dispatch({type: GET_COMMENT_ACTION, payload: response.data})
+        });
+    }
+}
+
+export function putComment(comment, token) {
+    return dispatch => {
+        axios.put(PUT_COMMENT, comment, {
+            headers: {'Authorization':`Bearer ${token}`}
+        }).then(response => {
+            dispatch({type: PUT_COMMENT_ACTION, payload: response.data})
+        });
+    }
+}
+
+export function deleteComment(id, token) {
+    return dispatch => {
+        axios.delete(DELETE_COMMENT_BY_ID(id), {
+            headers: {'Authorization':`Bearer ${token}`}
+        }).then(response => {
+            dispatch({type: DELETE_COMMENT_ACTION, payload: response.data})
+        });
+    }
+}
