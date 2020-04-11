152f29b16365e5f09ef4714259bfd014ba47f4a2
Hardelele <Hardelele@yahoo.com>
2020-04-11 12:03:47 +0300

    Add userInfo to state

--- frontend/src/redux/reducers/AuthReducer.js
+++ frontend/src/redux/reducers/AuthReducer.js
@@ -1,17 +1,19 @@
-import {AUTH_ACTION, REGISTER_ACTION} from "../actions/ActionTypes";
-import {postAuth, postRegister} from "../actions/AuthActions";
+import {AUTH_ACTION, REGISTER_ACTION, WHO_AM_I_ACTION} from "../actions/ActionTypes";
 
 const initialState = {
-    tokens:{}
+    tokens:{},
+    userInfo:{}
 };
 
 export function authReducer(state = initialState, action) {
     console.log(action);
     switch (action.type) {
         case AUTH_ACTION:
-            return {...state, tokens: action.payload};
+            return {...state, ...{tokens: action.payload}};
         case REGISTER_ACTION:
-            return {...state, tokens: action.payload};
+            return {...state, ...{tokens: action.payload}};
+        case WHO_AM_I_ACTION:
+            return {...state, ...{userInfo: action.payload}};
         default:
             return state;
     }
