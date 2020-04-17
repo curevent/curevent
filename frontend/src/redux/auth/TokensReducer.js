c13a84750a7834b01122566b50137bdffb320ae3
Hardelele <Hardelele@yahoo.com>
2020-04-17 16:28:15 +0300

    Rename to TokensReducer and small refactor

---
+++ frontend/src/redux/auth/TokensReducer.js
@@ -0,0 +1,29 @@
+import {
+    GET_AUTH_ACTION,
+    GET_REFRESH_ACTION,
+    GET_REGISTER_ACTION,
+    INVALIDATE_AUTH_ACTION,
+} from "../constants/ActionTypes";
+
+const initialState = {
+    tokens: {
+        access_token: null,
+        refresh_token: null
+    },
+    isAuth: false
+};
+
+export function tokensReducer(state = initialState, action) {
+    switch (action.type) {
+        case GET_AUTH_ACTION:
+        case GET_REGISTER_ACTION:
+        case GET_REFRESH_ACTION:
+            console.log(action);
+            return {...state, ...{tokens: action.payload, isAuth: true}};
+        case INVALIDATE_AUTH_ACTION:
+            console.log(action);
+            return {...state, ...{tokens: "", isAuth: false}};
+        default:
+            return state;
+    }
+}
