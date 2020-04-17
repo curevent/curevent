91f97929f9b1ecbc2c0d518b8d16ffc698138027
Hardelele <Hardelele@yahoo.com>
2020-04-17 16:25:36 +0300

    Make as class and change auth page routing

--- frontend/src/Application.js
+++ frontend/src/Application.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {AuthPage} from "./pages/auth/AuthPage";
 import './css/application.css'
 import ProfilePage from "./pages/profile/ProfilePage";
+import {connect} from "react-redux";
 
-function App() {
-  return (
-      <BrowserRouter>
-          <div className="application-container">
-              <Switch>
-                <Route path="/auth" exact component={AuthPage}/>
-                <Route path="/profile" component={ProfilePage}/>
-              </Switch>
-          </div>
-      </BrowserRouter>
-  );
+class Application extends Component {
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="application-container">
+                    <Switch>
+                        <Route path="/" exact component={AuthPage}/>
+                        <Route path="/profile" component={ProfilePage}/>
+                        {/*<Route path="/user/:id" component={UserPage}/>*/}
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
-export default App;
+export default connect(null, null)(Application);
