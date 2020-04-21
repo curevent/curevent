273eb2aa5c180ad1caf00375195736e129669499
Hardelele <Hardelele@yahoo.com>
2020-04-21 04:53:03 +0300

    Change routing and add logs

--- frontend/src/Application.js
+++ frontend/src/Application.js
@@ -2,18 +2,23 @@ import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import AuthPage from "./pages/auth/AuthPage";
 import './css/application.css'
-import ProfilePage from "./pages/profile/ProfilePage";
 import {connect} from "react-redux";
-import {refresh} from "./redux/auth/AuthActions";
-import {getRefresh} from "./redux/auth/AuthService";
+import {refresh} from "./redux/actions/AuthActions";
+import {getRefresh} from "./redux/services/AuthService";
 import {getTokens} from "./utils/localStorageUtils";
+import MyProfilePage from "./pages/profile/MyProfilePage";
+import ProfileByIdPage from "./pages/profile/ProfileByIdPage";
+import Header from "./components/Header";
 
 class Application extends Component {
 
     componentDidMount() {
         const tokens = getTokens();
         if (tokens.refresh != null) {
-            getRefresh().then(this.props.refresh);
+            getRefresh().then(tokens => {
+                console.log(tokens);
+                this.props.refresh(tokens);
+            });
         }
     }
 
@@ -21,10 +26,11 @@ class Application extends Component {
         return (
             <BrowserRouter>
                 <div className="application-container">
+                    <Header/>
                     <Switch>
                         <Route path="/" exact component={AuthPage}/>
-                        <Route path="/profile" component={ProfilePage}/>
-                        {/*<Route path="/user/:id" component={UserPage}/>*/}
+                        <Route path="/profile" component={MyProfilePage}/>
+                        <Route path="/user/:id" component={ProfileByIdPage}/>
                     </Switch>
                 </div>
             </BrowserRouter>
