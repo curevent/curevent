2acfb4f6f4f5efe3f93937543249d7cda85dc9e6
Hardelele <Hardelele@yahoo.com>
2020-04-25 19:33:02 +0300

    Add nav menu

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,26 +3,39 @@ import {connect, useDispatch} from "react-redux";
 import '../css/header.css'
 import {invalidateAuth} from "../redux/actions/AuthActions";
 import {invalidateLocalStorage} from "../utils/localStorageUtils";
+import {NavLink, Redirect} from "react-router-dom";
 
 const Header = ({isAuth}) => {
+
+    const AUTH_PAGE = "http://localhost:3000/";
 
     const dispatch = useDispatch();
 
     const logoutHandler = (ignore) => {
         invalidateLocalStorage();
         dispatch(invalidateAuth);
-        window.location.replace( window.location.href);
+        window.location.replace(AUTH_PAGE);
     };
 
+
     const logoutButton = () => {
-        return <button className="logout-button" onClick={logoutHandler}>Sign-out</button>
+        return <button className="header-button" onClick={logoutHandler}>Sign-out</button>
     };
 
     return (
         <div className="header-container">
             <div className="header-content">
                 <div className="header-logo">Curevent</div>
-                {isAuth && logoutButton()}
+                <div className="nav-menu">
+                    <NavLink className="nav-button" to="/profile">Profile</NavLink>
+                    <NavLink className="nav-button" to="/friends">Friends</NavLink>
+                    <NavLink className="nav-button" to="/timeline">Timeline</NavLink>
+                    <NavLink className="nav-button" to="/templates">Templates</NavLink>
+                </div>
+                <div className="header-auth-panel">
+                    {!isAuth && <Redirect to="/"/>}
+                    {isAuth && logoutButton()}
+                </div>
             </div>
         </div>
     );
