6ad359493134108ad7b523a24d7af9270d8ec2fb
Hardelele <Hardelele@yahoo.com>
2020-04-29 12:52:14 +0300

    Create TemplatesRepository Component

---
+++ frontend/src/components/TemplatesRepository.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import '../css/templates.css'
+import {expand, minimize} from "../redux/actions/TemplateActions";
+import ExpandPanel from "./ExpandPanel";
+import TemplatesList from "./TemplatesList";
+
+class TemplatesRepository extends Component {
+    render() {
+
+        const expandHandler = (event) => {
+            if (this.props.isMinimized) {
+                this.props.expand()
+            } else {
+                this.props.minimize()
+            }
+        };
+
+        return (
+            <div className="templates-board">
+                <ExpandPanel/>
+                {!this.props.isMinimized &&
+                    <div className="repository-panel">
+                        <TemplatesList/>
+                    </div>
+                }
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        templates: state.templates,
+        isMinimized: state.repo.isMinimized
+    }
+};
+
+const mapDispatchToProps = {
+    expand,
+    minimize
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TemplatesRepository);
