285d135b29ce92382a4da0516f6dec93542a00c2
Hardelele <Hardelele@yahoo.com>
2020-04-29 12:51:34 +0300

    Add ExpandPanel

---
+++ frontend/src/components/ExpandPanel.js
@@ -0,0 +1,35 @@
+import {expand, minimize} from "../redux/actions/TemplateActions";
+import {connect} from "react-redux";
+import React, {Component} from "react";
+
+class ExpandPanel extends Component {
+    render() {
+
+        const expandHandler = (event) => {
+            if (this.props.isMinimized) {
+                this.props.expand()
+            } else {
+                this.props.minimize()
+            }
+        };
+
+        return (
+            <div className="minimized-panel">
+                <button className="expand-button" onClick={expandHandler}/>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        isMinimized: state.repo.isMinimized
+    }
+};
+
+const mapDispatchToProps = {
+    expand,
+    minimize
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpandPanel);
