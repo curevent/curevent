a8cf9220111b03c4f7caad230334dd32dfec0149
Hardelele <Hardelele@yahoo.com>
2020-05-09 03:32:38 +0300

    Add SearchMenu

--- frontend/src/components/header/Search.js
+++ frontend/src/components/header/Search.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import SearchMenu from "./SearchMenu";
 
 export const Search = () => {
-  return (
-      <div className="search-container">
-          <div className="search-icon"/>
-          <input className="search-input"/>
-      </div>
-  );
+
+    const [search, setSearch] = useState();
+
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    const toggleMenu = (event) => {
+        event.preventDefault();
+        setMenuVisible(prev => (!prev));
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+    };
+
+    const enterKeyHandler = (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            submitHandler(event);
+        }
+    };
+
+    const changeInputHandler = event => {
+        event.persist();
+        setSearch(event.target.value);
+    };
+
+    return (
+        <div className="search-container">
+            <div className="search-icon" onClick={submitHandler}/>
+            <input
+                className="search-input"
+                onKeyPress={enterKeyHandler}
+                value={search}
+                onChange={changeInputHandler}
+                placeholder="Enter username or full name"
+            />
+            {menuVisible && <SearchMenu/>}
+        </div>
+    );
 };
---
+++ frontend/src/components/header/SearchMenu.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import {connect} from "react-redux";
+
+const SearchMenu = () => {
+
+    return (
+        <div className="search-menu">
+            123
+        </div>
+    );
+};
+
+const mapStateToProps = state => {
+    return {
+
+    }
+};
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchMenu);
